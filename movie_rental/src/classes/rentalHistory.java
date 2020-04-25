1efff7a5a7c5ece58fbe3e67ba76493eefcd3edd
Irfanmalik26 <56300437+Irfanmalik26@users.noreply.github.com>
2020-04-25 17:17:11 +0500

    Add files via upload

---
+++ movie_rental/src/classes/rentalHistory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+
+public class rentalHistory {
+    int historyID;
+    customer c;
+    title t;
+    //DateTime d;
+    boolean active;
+    
+    
+}
