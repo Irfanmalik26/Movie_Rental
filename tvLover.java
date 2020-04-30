f309820d850ce255f9b8e7055efdbc74fa2d3878
Shahbaz906 <61193882+Shahbaz906@users.noreply.github.com>
2020-04-30 21:49:23 +0500

    Add files via upload

---
+++ tvLover.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+
+public class tvLover extends subscription{
+   
+    //default constructor()
+    public tvLover(){
+     this.subscriptionID=2;
+     this.subscriptionName="TV Lover";
+    }
+    
+}
---
+++ videoLover.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+
+public class videoLover extends subscription{
+  
+    //default constructor()
+    public videoLover(){
+     this.subscriptionID=3;
+     this.subscriptionName="Video Lover";
+    }
+}
